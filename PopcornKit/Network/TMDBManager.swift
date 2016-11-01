21df83edc036b8ec8de6988e5e253504e2668753
User <Mark@MacBook-Air.local>
2016-11-01 18:53:18 +0000

    Access control fixes

--- PopcornKit/Network/TMDBManager.swift
+++ PopcornKit/Network/TMDBManager.swift
@@ -4,7 +4,7 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 
-class TMDBManager: NetworkManager {
+open class TMDBManager: NetworkManager {
     
     /// Creates new instance of TMDBManager class
     open static let shared = TMDBManager()
